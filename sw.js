fbd1679e1931cf177319c5c282db56400e2250e8
nagapersada <kharismanagapersada@gmail.com>
2025-12-04 15:40:25 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'dvteam-v14';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png',
+  './DVTMNP-PRO(2).mp3'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
